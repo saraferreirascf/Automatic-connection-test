4197cb3a677f92ffdbb73e34c72ca8ee16c87944
Sara Ferreira <44060413+saraferreirascf@users.noreply.github.com>
2021-03-04 15:44:47 +0000

    conn test added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const {Builder, By, Key, util} = require ("selenium-webdriver");
-const html2canvas = require('html2canvas');
-const puppeteer = require("puppeteer");
+//const html2canvas = require('html2canvas');
+//const puppeteer = require("puppeteer");
 
 async function test_website() {
     let driver = await new Builder().forBrowser("firefox").build();
@@ -23,6 +23,13 @@ async function test_mail() {
     console.log(driver)
 }
 
+async function test_conn() {
+  let driver = await new Builder().forBrowser("firefox").build();
+  await driver.get("http://conn.internet.nl/connection/");
+  //await driver.findElement(By.className("email")).sendKeys("test@teste-osse-isoc-amen.pt",Key.RETURN);
+  
+}
+
 /* async function printPDF() {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
@@ -42,6 +49,7 @@ async function test_mail() {
 } */
 
 
-test_website()
+//test_website()
 //printPDF();
-test_mail()
+//test_mail()
+test_conn();
